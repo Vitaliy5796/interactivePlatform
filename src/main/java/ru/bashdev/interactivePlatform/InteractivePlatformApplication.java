535ca695b8a4bd2b982a848d3d49d58a43114dd3
Vitaliy Sidorov <sid0r0vvitaliy@yandex.ru>
2023-05-18 17:15:15 +0300

    Initial commit

---
+++ src/main/java/ru/bashdev/interactivePlatform/InteractivePlatformApplication.java
@@ -0,0 +1,13 @@
+package ru.bashdev.interactivePlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractivePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractivePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bashdev/interactivePlatform/InteractivePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bashdev.interactivePlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractivePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
